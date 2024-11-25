12ab6896fa6864c634db9d18d312330b9dc30529
thegr8nick <kudashevnikitushka@yandex.ru>
2024-11-25 22:24:37 +0400

    change regex for height, longitude, locale_code and time

--- lab_3/main.py
+++ lab_3/main.py
@@ -13,15 +13,15 @@ def is_row_valid(row: dict[str, str]) -> bool:
     """Tests a CSV string against the specified regular expressions."""
     processors = {
         "email": "^[\\w\\.-]+@[\\w\\.-]+\\.\\w+$",
-        "height": "^\\d\\.\\d{2}$",
+        'height': "^[1-2]\\.\\d{2}$",
         "snils": "^\\d{11}$",
         "passport": "^\\d{2} \\d{2} \\d{6}$",
         "occupation": "^[А-Яа-яA-Za-z\\s\\-]+$",
-        "longitude": "^-?\\d+(\\.\\d+)?$",
+        "longitude": "^(-?(180(\\.0{1,6})?|(1[0-7]\\d|\\d{1,2})(\\.\\d{1,6})?))$",
         "hex_color": "^#[0-9a-fA-F]{6}$",
         "issn": "^\\d{4}-\\d{4}$",
-        "locale_code": "^[a-z]{2}-[A-Z]{2}$",
-        "time": "^\\d{2}:\\d{2}:\\d{2}\\.\\d{6}$"
+        "locale_code": "^[a-z]{2}(-[a-z]{2}?)?$",
+        "time": "^(2[0-3]|[01]\\d):([0-5]\\d):([0-5]\\d)(\\.\\d{1,6})?$"
     }
     for field_name, processor in processors.items():
         if re.fullmatch(processor, row[field_name]) is None:
